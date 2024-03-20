96d5109d172362cf0a88f75dc5a207c724f38825
heleopless50 <heleopless@yahoo.com>
2024-03-20 17:17:40 +0200

    REMOVE SSL

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -39,12 +39,12 @@ if (app.Environment.IsDevelopment())
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
-app.UseHealthChecks("/health");
+//app.UseHealthChecks("/health");
 app.UseCors("CorsPolicy");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseForwardedHeaders();
 
