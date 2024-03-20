9a68dc5655ceb281cc1c33474acb54ea7752dab3
heleopless50 <heleopless@yahoo.com>
2024-03-20 23:58:22 +0200

    fsgsdgsdfg

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddWebServices();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -44,9 +45,9 @@ else
 
 app.UseHealthChecks("/health");
 app.UseCors("AllowAll");
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseForwardedHeaders();
+app.UseHttpsRedirection();
 
 
 app.UseSwaggerUi(settings =>
